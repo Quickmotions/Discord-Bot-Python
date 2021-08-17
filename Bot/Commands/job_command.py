8b11aa4db186f36be63bda9cbc9844f31221022a
Quickmotions <fergusbhaak@gmail.com>
2021-08-17 15:50:10 +0100

    added work commands to allow users to get jobs and gain money, need cooldown module to handle user cooldowns in order for job command to function fully

---
+++ Bot/Commands/job_command.py
@@ -0,0 +1,4 @@
+def job_c(*args):  # 0 = this user_data, 1 = Command Class, 2 = all user data, 3 = extra args in list
+    # work command options: job get
+
+    if len(args[3]) > 1:  # test for arg
