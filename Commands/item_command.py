72cd190ff9b0c407d768cb90a6d40b86e83af2cb
Quickmotions <fergusbhaak@gmail.com>
2021-08-26 12:18:07 +0100

    fixed issue where items in item command list wouldnt work due to capitalisation

--- Commands/item_command.py
+++ Commands/item_command.py
@@ -33,7 +33,7 @@ def item_c(*args):  # 0 = this user_data, 1 = Command Class, 2 = all user data, 
     if len(args[3]) > 0:
         for item in item_list:
             if args[3][0] == item.lower():
-                return f"Info for {args[3][0].title()}:\n{item_list[args[3][0]]}"
+                return f"Info for {item}:\n{item_list[item]}"
     else:
         return f"Incorrect use of item:\ntry 'item (item-name)'"
 
