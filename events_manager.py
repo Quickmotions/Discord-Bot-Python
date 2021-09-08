4dc23fe39bd1920e80e934c1898ce2b364e0a0d9
Quickmotions <fergusbhaak@gmail.com>
2021-09-08 11:02:23 +0100

    Added stata buffs to mobs depending on party size

--- events_manager.py
+++ events_manager.py
@@ -77,7 +77,10 @@ def start_combat(user, users, mob, battle_type):
 
                 user_data.append([hp, max_hp, 0, draw_card_deck(user_party[0][0], users), 0, dodge])
     event_data.append(user_data)
-    event_data.append([mob[0], mob[1], mob[2], mob[2], mob[3], mob[4]])
+
+    mob_max = mob[2] * (0.6 * len(user_party))
+    mob_dmg = mob[3] * (0.2 * len(user_party))
+    event_data.append([mob[0], mob[1], mob_max, mob_max, mob_dmg, mob[4]])
 
     # info about EVENT_DATA: ----------------------------
     # turn, battle_type, party, user_data, mob_data
